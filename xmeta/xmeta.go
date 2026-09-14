678dd6a5d0150d83e8aea0bdc5d56f4ab3f5253b
Tachera Sasi <tacherasasi@gmail.com>
2026-09-14 09:51:49 +0300

    add xmeta Get, Remove, List logic

--- xmeta/xmeta.go
+++ xmeta/xmeta.go
@@ -2,13 +2,26 @@ package xmeta
 
 import "github.com/pkg/xattr"
 
+// Set attaches attrVal to filepath under attrKey.
 func Set(filepath, attrKey string, attrVal []byte) error {
 	err := xattr.Set(filepath, attrKey, attrVal)
 	return err
 }
 
-func Get() {}
+// Get retrieves the value stored under attrKey on filepath.
+func Get(filepath, attrKey string) ([]byte, error) {
+	val, err := xattr.Get(filepath, attrKey)
+	return val, err
+}
 
-func Remove() {}
+// Remove deletes attrKey from filepath.
+func Remove(filepath, attrKey string) error {
+	err := xattr.Remove(filepath, attrKey)
+	return err
+}
 
-func List() {}
+// List returns all attribute keys set on filepath.
+func List(filepath string) ([]string, error) {
+	attrs, err := xattr.List(filepath)
+	return attrs, err
+}
